de93dde875e5b53d7e86b908646413bbe40c2017
gcarpinelli76 <g.carpinelli94@gmail.com>
2026-02-13 13:33:13 +0100

    Create dati.js

---
+++ dati.js
@@ -0,0 +1,7 @@
+/ Archivio fisso delle cartelle (Esempio prime 3) [cite: 2026-02-12]
+const ARCHIVIO_FISSO = [
+    [[null,15,null,33,null,55,null,72,81],[1,null,22,null,40,null,61,null,88],[8,null,28,44,null,59,null,77,null]], // Cartella 1
+    [[5,null,21,null,42,null,63,70,null],[null,12,null,35,48,51,null,null,85],[9,19,null,null,null,58,66,null,90]], // Cartella 2
+    [[2,11,null,null,41,null,null,71,82],[null,null,25,38,null,50,65,79,null],[7,18,null,47,null,56,null,null,89]]  // Cartella 3
+    // Aggiungerò le altre 997 se necessario
+];
